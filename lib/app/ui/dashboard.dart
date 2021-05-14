51d03fffc765962228c3558e0aa6ffb265547e29
Justin <justinMorettoMTL@gmail.com>
2021-05-14 11:31:53 -0400

    started building basic dashboard UI

---
+++ lib/app/ui/dashboard.dart
@@ -0,0 +1,46 @@
+import 'package:coronavirus_rest_api_flutter_course/app/repositories/data_repository.dart';
+import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
+import 'package:coronavirus_rest_api_flutter_course/app/ui/endpoint_card.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  int _cases;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateData();
+  }
+
+  Future<void> _updateData() async {
+    final dataRepository = Provider.of<DataRepository>(context, listen: false);
+    final cases = await dataRepository.getEndpointData(Endpoint.cases);
+    setState(() => _cases = cases);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Covid-19 Tracker'),
+      ),
+      body: RefreshIndicator(
+        onRefresh: _updateData,
+        child: ListView(
+          children: <Widget>[
+            EndpointCard(
+              endpoint: Endpoint.cases,
+              value: _cases,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
