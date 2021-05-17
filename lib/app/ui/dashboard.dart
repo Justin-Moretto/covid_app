efc80707f9fa28b76b90eab639d2ef8e20c8e02c
Justin <justinMorettoMTL@gmail.com>
2021-05-17 15:42:36 -0400

    added 'Last updaated: date' text and formatted date and case numbers

--- lib/app/ui/dashboard.dart
+++ lib/app/ui/dashboard.dart
@@ -2,6 +2,7 @@ import 'package:coronavirus_rest_api_flutter_course/app/repositories/data_reposi
 import 'package:coronavirus_rest_api_flutter_course/app/repositories/endpoints_data.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/ui/endpoint_card.dart';
+import 'package:coronavirus_rest_api_flutter_course/app/ui/last_updated_status_text.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -27,6 +28,11 @@ class _DashboardState extends State<Dashboard> {
 
   @override
   Widget build(BuildContext context) {
+    final formatter = LastUpdatedDateFormatter(
+      lastUpdated: _endpointsData != null
+          ? _endpointsData.values[Endpoint.cases].date
+          : null,
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text('Covid-19 Tracker'),
@@ -35,11 +41,14 @@ class _DashboardState extends State<Dashboard> {
         onRefresh: _updateData,
         child: ListView(
           children: <Widget>[
+            LastUpdatedStatusText(
+              text: formatter.lastUpdatedStatusText(),
+            ),
             for (var endpoint in Endpoint.values)
               EndpointCard(
                 endpoint: endpoint,
                 value: _endpointsData != null
-                    ? _endpointsData.values[endpoint]
+                    ? _endpointsData.values[endpoint].value
                     : null,
               ),
           ],
