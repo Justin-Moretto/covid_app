d3d1fd551a8df8444a1feda83ba42863226af106
Justin <justinMorettoMTL@gmail.com>
2021-05-17 11:47:56 -0400

    added color and assets to UI, refactored data endpoints

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,3 +1,4 @@
+import 'package:coronavirus_rest_api_flutter_course/app/repositories/endpoints_data.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_service.dart';
 import 'package:flutter/material.dart';
@@ -9,21 +10,54 @@ class DataRepository {
 
   String _accessToken;
 
-  Future<int> getEndpointData(Endpoint endpoint) async {
+  Future<int> getEndpointData(Endpoint endpoint) async =>
+      await _getDataRefreshingToken<int>(
+        onGetData: () => apiService.getEndpointData(
+            accessToken: _accessToken, endpoint: endpoint),
+      );
+
+  Future<EndpointsData> getAllEndpointsData() async =>
+      await _getDataRefreshingToken<EndpointsData>(
+        onGetData: _getAllEndpointsData,
+      );
+
+  Future<T> _getDataRefreshingToken<T>({Future<T> Function() onGetData}) async {
     try {
       if (_accessToken == null) {
         _accessToken = await apiService.getAccessToken();
       }
-      return await apiService.getEndpointData(
-          accessToken: _accessToken, endpoint: endpoint);
+      return await onGetData();
     } on Response catch (response) {
       // if unauthorized, get access token again
       if (response.statusCode == 401) {
         _accessToken = await apiService.getAccessToken();
-        return await apiService.getEndpointData(
-            accessToken: _accessToken, endpoint: endpoint);
+        return await onGetData();
       }
       rethrow;
     }
+  }
+
+  Future<EndpointsData> _getAllEndpointsData() async {
+    final values = await Future.wait([
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.cases),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.casesSuspected),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.casesConfirmed),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.deaths),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.recovered),
+    ]);
+    return EndpointsData(
+      values: {
+        Endpoint.cases: values[0],
+        Endpoint.casesSuspected: values[1],
+        Endpoint.casesConfirmed: values[2],
+        Endpoint.deaths: values[3],
+        Endpoint.recovered: values[4],
+      },
+    );
   }
 }
--- lib/app/ui/endpoint_card.dart
+++ lib/app/ui/endpoint_card.dart
@@ -1,28 +1,67 @@
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:flutter/material.dart';
 
+class EndpointCardData {
+  EndpointCardData(this.title, this.assetName, this.color);
+  final String title;
+  final String assetName;
+  final Color color;
+}
+
 class EndpointCard extends StatelessWidget {
   const EndpointCard({Key key, this.endpoint, this.value}) : super(key: key);
   final Endpoint endpoint;
   final int value;
 
+  static Map<Endpoint, EndpointCardData> _cardsData = {
+    Endpoint.cases:
+        EndpointCardData('Cases', 'assets/count.png', Color(0xFFFFF492)),
+    Endpoint.casesSuspected: EndpointCardData(
+        'Suspected Cases', 'assets/suspect.png', Color(0xFFEEDA28)),
+    Endpoint.casesConfirmed: EndpointCardData(
+        'Confirmed cases', 'assets/fever.png', Color(0xFFE99600)),
+    Endpoint.deaths:
+        EndpointCardData('Deaths', 'assets/death.png', Color(0xFFE40000)),
+    Endpoint.recovered:
+        EndpointCardData('Recovered', 'assets/patient.png', Color(0xFF70A901)),
+  };
+
   @override
   Widget build(BuildContext context) {
+    final cardData = _cardsData[endpoint];
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
       child: Card(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Text(
-                'Cases',
-                style: Theme.of(context).textTheme.headline,
+                cardData.title,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline
+                    .copyWith(color: cardData.color),
               ),
-              Text(
-                value != null ? value.toString() : '',
-                style: Theme.of(context).textTheme.display1,
+              SizedBox(height: 4),
+              SizedBox(
+                height: 52,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      cardData.assetName,
+                      color: cardData.color,
+                    ),
+                    Text(
+                      value != null ? value.toString() : '',
+                      style: Theme.of(context).textTheme.display1.copyWith(
+                          color: cardData.color, fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
